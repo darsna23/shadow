03c709be407f834bbcccad853386e1b3a31d8317
DarsnaSaravanakumar <darsnasaravanakumar232006@gmail.com>
2026-08-26 00:01:54 +0530

    improve application consistency

--- shadowbase-backend/src/main/java/com/infotact/shadowbase/ShadowBaseApplication.java
+++ shadowbase-backend/src/main/java/com/infotact/shadowbase/ShadowBaseApplication.java
@@ -2,7 +2,7 @@ package com.infotact.shadowbase;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+// spring boot application
 @SpringBootApplication
 public class ShadowBaseApplication {
     public static void main(String[] args) {
